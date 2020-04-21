a929497e83f58b29c31dc6f211f5df88825ddc70
Sam Levy <slevy239@gmail.com>
2020-04-20 23:19:52 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Main from './Pages/Main/Main'
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <div id='App'>
+        <Router>
+          <div>
+            <Switch>
+              <Route exact path='/' componen={Main} />
+            </Switch>
+          </div>
+        </Router>
         <Main />
       </div>
     </div>
