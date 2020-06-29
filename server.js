6f2fc425eaa92ab28a1008a6c2bffac8ea07aec5
Sam Levy <slevy239@gmail.com>
2020-06-29 15:53:17 -0400

    backend

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 //Techonology needed to begin and start app
 const express = require("express");
 const app = express();
+// const routes = require("./routes");
+// const db = require('./models')
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//For connecting MongoDB
+// const mongoose = require("mongoose");
+// Connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ReactScraper");
 
 
 // Serve up static assets (usually on heroku)
